effa20294b64c720684e83cfa0aa79ca4f2f9911
Jacob Godserv <jacobgodserv@gmail.com>
2015-11-02 21:41:42 +0000

    Fixed paths in merge-kde-staging.sh

--- merge-kde-staging.sh
+++ merge-kde-staging.sh
@@ -4,4 +4,4 @@ set -e
 eval `keychain --eval --agents ssh id_rsa`
 cd /home/jacob/funtoo-git/gentoo && git pull
 cd /home/jacob/funtoo-git/gentoo-kde-overlay && git pull
-/home/jacob/kde-funtoo-scripts/scripts/merge-kde-staging.py
+/home/jacob/kde-funtoo-scripts/merge-kde-staging.py
